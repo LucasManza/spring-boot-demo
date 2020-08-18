ae26ed5613ab15a2c82c38242d4ca725a4c26a70
Lucas Manzanelli <lucas.manzanelli@ing.austral.edu.ar>
2020-08-18 11:17:29 -0300

    Spring boot demo connections in progress.

---
+++ src/main/java/com/example/demo/OnStart.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+
+import com.example.demo.models.UserModel;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class OnStart {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public OnStart(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @EventListener
+    public void appReady(ApplicationReadyEvent event) {
+        Boolean init = event.getApplicationContext().getEnvironment().getProperty("initdb", boolean.class);
+        if (init == null || init) {
+            try {
+                // users
+                UserModel user1 = userRepository.save(new UserModel("Lucas", "Manzanelli"));
+
+            } catch (Exception ignored) {
+            }
+        }
+
+    }
+}
--- src/main/java/com/example/demo/models/UserModel.java
+++ src/main/java/com/example/demo/models/UserModel.java
@@ -9,14 +9,15 @@ public class UserModel implements Identifiable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private int id;
+    @Column
     private String name;
+    @Column
     private String lastName;
 
     public UserModel() {
     }
 
-    public UserModel(int id, String name, String lastName) {
-        this.id = id;
+    public UserModel(String name, String lastName) {
         this.name = name;
         this.lastName = lastName;
     }
@@ -24,5 +25,13 @@ public class UserModel implements Identifiable {
     @Override
     public int getId() {
         return this.id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 }
